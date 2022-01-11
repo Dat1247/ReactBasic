1b08e8f44bdccfe4105927ef90ad9cf6bc776428
Ho Dat <hoquandat1247@gmail.com>
2022-01-11 16:15:08 +0700

    Du an dat ve xem phim

--- src/App.js
+++ src/App.js
@@ -1,38 +1,7 @@
 import "./App.css";
-import BaiTapTongHop from "./BaiTapTongHop/BaiTapTongHop";
-import CompareDemo from "./LodashComponent/CompareDemo";
-import FillDemo from "./LodashComponent/FillDemo";
-import FlattenDemo from "./LodashComponent/FlattenDemo";
-import JoinDemo from "./LodashComponent/JoinDemo";
-import LastFirstDemo from "./LodashComponent/LastFirstDemo";
-import SortByDemo from "./LodashComponent/SortByDemo";
-import UniqDemo from "./LodashComponent/UniqDemo";
-import BaiTapLayout from "./TailwindComponent/BaiTapLayout";
-import CustomCSS from "./TailwindComponent/CustomCSS";
-import DemoFlex from "./TailwindComponent/DemoFlex";
-import DemoGrid from "./TailwindComponent/DemoGrid";
-import DemoPaddingMargin from "./TailwindComponent/DemoPaddingMargin";
-import DemoWidthHeight from "./TailwindComponent/DemoWidthHeight";
 
 function App() {
-	return (
-		<div>
-			{/* <DemoGrid /> */}
-			{/* <DemoPaddingMargin /> */}
-			{/* <DemoWidthHeight /> */}
-			{/* <DemoFlex /> */}
-			{/* <CustomCSS /> */}
-			{/* <BaiTapLayout /> */}
-			{/* <JoinDemo /> */}
-			{/* <LastFirstDemo /> */}
-			{/* <SortByDemo /> */}
-			{/* <FillDemo /> */}
-			{/* <UniqDemo /> */}
-			{/* <FlattenDemo /> */}
-			{/* <CompareDemo /> */}
-			<BaiTapTongHop />
-		</div>
-	);
+	return <div></div>;
 }
 
 export default App;
